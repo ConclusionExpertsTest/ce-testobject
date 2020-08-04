624f677f6537d66fc6519fe92bcc9d482bc30058
Bart Bouman <bart.bouman@conclusion.nl>
2020-08-04 11:13:05 +0200

    fix e2e tests locally

--- e2e-test/src/test/java/CeUsersTests/CeUsersTest.java
+++ e2e-test/src/test/java/CeUsersTests/CeUsersTest.java
@@ -10,6 +10,7 @@ import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import usersapplication.domain.NewUpdateUsers;
 import usersapplication.domain.Users;
 
 import java.io.File;
@@ -37,19 +38,29 @@ public class CeUsersTest {
     @Test
     public void CrudActions() throws JsonProcessingException {
         // Arrange
+        NewUpdateUsers postUsers1 = NewUpdateUsers.builder().isActive(true).firstName("Harry")
+                .lastName("Wit, de").address("Straat 2").occupation("TAE").workingConditionsId(1).build();
+        NewUpdateUsers postUsers2 = NewUpdateUsers.builder().isActive(true).firstName("Jos")
+                .lastName("Lelijk").address("Weg 400").occupation("TAE").workingConditionsId(1).build();
+        NewUpdateUsers postUsers3 = NewUpdateUsers.builder().isActive(false).firstName("Jos")
+                .lastName("Lelijk").address("Weg 400").occupation("TAE").workingConditionsId(1).build();
         Users users1 = Users.builder().id(1L).isActive(true).firstName("Harry")
                 .lastName("Wit, de").address("Straat 2").occupation("TAE").workingConditionsId(1).build();
         Users users2 = Users.builder().id(2L).isActive(true).firstName("Jos")
                 .lastName("Lelijk").address("Weg 400").occupation("TAE").workingConditionsId(1).build();
         Users users3 = Users.builder().id(3L).isActive(false).firstName("Jos")
                 .lastName("Lelijk").address("Weg 400").occupation("TAE").workingConditionsId(1).build();
+        NewUpdateUsers putUpdatedUsers1 = NewUpdateUsers.builder().isActive(true).firstName("Johan")
+                .lastName("Rode, de").address("Steegje 22").occupation("FAB").workingConditionsId(1).build();
         Users updatedUsers1 = Users.builder().id(1L).isActive(true).firstName("Johan")
                 .lastName("Rode, de").address("Steegje 22").occupation("FAB").workingConditionsId(1).build();
 
+        String blabla = objectMapper.writeValueAsString(postUsers1);
+
         // Act - POST
-        Response postResponse1 = CeUsersEndpoints.postNewCeUser(objectMapper.writeValueAsString(users1));
-        Response postResponse2 = CeUsersEndpoints.postNewCeUser(objectMapper.writeValueAsString(users2));
-        Response postResponse3 = CeUsersEndpoints.postNewCeUser(objectMapper.writeValueAsString(users3));
+        Response postResponse1 = CeUsersEndpoints.postNewCeUser(objectMapper.writeValueAsString(postUsers1));
+        Response postResponse2 = CeUsersEndpoints.postNewCeUser(objectMapper.writeValueAsString(postUsers2));
+        Response postResponse3 = CeUsersEndpoints.postNewCeUser(objectMapper.writeValueAsString(postUsers3));
 
         // Assert - POST
         assertThat(postResponse1.statusCode()).isEqualTo(HttpStatus.SC_CREATED);
@@ -83,7 +94,7 @@ public class CeUsersTest {
         assertThat(getResponse3.body().print()).isEqualTo(objectMapper.writeValueAsString(users3));
 
         // Act - PUT & GET (by id)
-        Response putResponse = CeUsersEndpoints.putCeUser("1", objectMapper.writeValueAsString(updatedUsers1));
+        Response putResponse = CeUsersEndpoints.putCeUser("1", objectMapper.writeValueAsString(putUpdatedUsers1));
         Response getUpdatedResponse1 = CeUsersEndpoints.getById(users1.getId().toString());
 
         // Assert - PUT & GET (by id)
