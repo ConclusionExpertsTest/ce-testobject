04289c3f5efedaeb14e6e3435435eb58967d8359
Bart Bouman <bartbouman22@gmail.com>
2020-09-09 16:44:56 +0200

    Updated users repo tests so they are better readable

--- users-application/src/test/java/usersapplication/unit/repository/UsersRepositoryTest.java
+++ users-application/src/test/java/usersapplication/unit/repository/UsersRepositoryTest.java
@@ -1,103 +1,105 @@
 package usersapplication.unit.repository;
 
+import org.junit.jupiter.api.*;
+import org.springframework.dao.DataIntegrityViolationException;
+import usersapplication.common.ReplaceCamelCase;
 import usersapplication.domain.Users;
 import usersapplication.repository.UsersRepository;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.test.context.jdbc.Sql;
-
-import java.util.Collection;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
+@DisplayNameGeneration(ReplaceCamelCase.class)
+@DisplayName("Users repository tests")
 class UsersRepositoryTest {
 
     @Autowired
     private UsersRepository usersRepository;
 
-    @Test
-    @Sql("data.sql")
-    public void findCeUserById() {
-        // Act
-        Optional<Users> ceUsers = usersRepository.findById(100L);
+    private Users newActiveUser = Users.builder()
+            .firstName("test 1")
+            .isActive(true)
+            .lastName("de tester 1")
+            .address("testweg 456")
+            .occupation("what he does all day").build();
 
-        // Assert
-        assertThat(ceUsers).isNotNull();
-    }
+    @Nested
+    public class givenAnEmptyUsersRepository {
 
-    @Test
-    public void createUserAndAssertIdCountingAnsResult() {
-        // Arrange
-        Users newUsers1 = Users.builder()
-                .firstName("test 1")
-                .isActive(true)
-                .lastName("de tester 1")
-                .address("testweg 456")
-                .occupation("what he does all day").build();
+        @BeforeEach
+        public void setup() {
+            usersRepository.deleteAll();
+        }
 
-        Users newUsers2 = Users.builder()
-                .firstName("test 2")
-                .isActive(true)
-                .lastName("de tester 2")
-                .address("testweg 456")
-                .occupation("what he does all day").build();
+        @Test
+        public void thenSearchingForAllUsersShouldReturnAnEmptyCollection() {
+            assertThat(usersRepository.findAll()).isEmpty();
+        }
 
-        usersRepository.save(newUsers1);
-        usersRepository.save(newUsers2);
+        @Test
+        public void thenSearchingForAllActiveUsersShouldReturnAnEmptyCollection() {
+            assertThat(usersRepository.findAllByIsActive()).isEmpty();
+        }
 
-        // Act
-        Optional<Users> ceUsers1 = usersRepository.findById(1L);
-        Optional<Users> ceUsers2 = usersRepository.findById(2L);
+        @Test
+        public void thenSearchingForAllInactiveUsersShouldReturnAnEmptyCollection() {
+            assertThat(usersRepository.findAllByIsInActive()).isEmpty();
+        }
 
-        // Assert
-        assertThat(ceUsers1.get().getFirstName()).isEqualTo(newUsers1.getFirstName());
-        assertThat(ceUsers1.get().getLastName()).isEqualTo(newUsers1.getLastName());
-        assertThat(ceUsers1.get().getAddress()).isEqualTo(newUsers1.getAddress());
-        assertThat(ceUsers1.get().getOccupation()).isEqualTo(newUsers1.getOccupation());
+        @Test
+        public void thenAddingAnEmptyUserWillResultInException() {
+            assertThrows(DataIntegrityViolationException.class, () -> usersRepository.save(Users.builder().build()));
+        }
 
-        assertThat(ceUsers2.get().getFirstName()).isEqualTo(newUsers2.getFirstName());
-        assertThat(ceUsers2.get().getLastName()).isEqualTo(newUsers2.getLastName());
-        assertThat(ceUsers2.get().getAddress()).isEqualTo(newUsers2.getAddress());
-        assertThat(ceUsers2.get().getOccupation()).isEqualTo(newUsers2.getOccupation());
-    }
+        @Nested
+        public class whenAnActiveUserIsAdded {
 
-    @Test
-    @Sql("data.sql")
-    public void findUserByIsActive() {
-        // Act
-        Collection<Users> users = usersRepository.findAllByIsActive();
-        Collection<Users> usersInActive = usersRepository.findAllByIsInActive();
+            @BeforeEach
+            public void setup() {
+                usersRepository.saveAndFlush(newActiveUser);
+            }
 
-        // Assert
-        assertThat(users).isNotEmpty();
-        assertThat(usersInActive).isNotEmpty();
-    }
+            @Test
+            public void thenSearchingForAllUsersShouldReturnACollectionWithOneUser() {
+                assertThat(usersRepository.findAll())
+                        .hasSize(1)
+                        .hasAtLeastOneElementOfType(Users.class);
+            }
 
-    @Test
-    public void findUserAndAssertEmptyResponseIsGivenInsteadOfException() {
-        // Act
-        Optional<Users> ceUsers = usersRepository.findById(1L);
+            @Test
+            public void thenSearchingForAllActiveUsersShouldReturnACollectionWithOneUser() {
+                assertThat(usersRepository.findAllByIsActive())
+                        .hasSize(1)
+                        .hasAtLeastOneElementOfType(Users.class);
+            }
 
-        // Assert
-        assertThat(ceUsers).isEmpty();
-    }
+            @Test
+            public void thenSearchingForAllInactiveUsersShouldReturnAnEmptyCollection() {
+                assertThat(usersRepository.findAllByIsInActive()).isEmpty();
+            }
 
-    @Test
-    public void tryCreateCeUserWithEmptyBody() {
-        // Act
-        assertThrows(DataIntegrityViolationException.class, () -> {
-            usersRepository.save(Users.builder().build());
-        });
+            @Test
+            public void thenSearchingForUserWithIdZeroShouldReturnAUser() {
+                assertThat(usersRepository.findById(1L)).isNotEmpty().containsInstanceOf(Users.class);
+            }
 
-        Optional<Users> ceUsers = usersRepository.findById(1L);
+            @Test
+            public void thenSearchingForUserWithIdOneShouldReturnAnEmptyUser() {
+                assertThat(usersRepository.findById(2L)).isEmpty();
+            }
 
-        // Assert
-        assertThat(ceUsers).isEmpty();
+            @Test
+            @Disabled
+            public void thenTheUserCanBeDeleted() {
+                usersRepository.deleteById(1L);
+                assertThat(usersRepository.findById(1L)).isEmpty();
+            }
+
+        }
+
     }
 
 }
